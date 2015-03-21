c0c5ebcbeeeca540f4e9eac96b12e790bc6f0615
ben_cripps <ben@benjamincripps.com>
2015-03-21 15:49:03 -0500

    more work on mobile

--- grunt/sass.js
+++ grunt/sass.js
@@ -1,22 +1,20 @@
 module.exports = {
-    // Development settings
+
     dev: {
         options: {
             outputStyle: 'nested',
             sourceMap: true
         },
         files: [{
-            expand: true,
-            cwd: 'app/public/dev/styles',
-            src: ['*.scss'],
-            dest: 'app/public/min/styles',
-            ext: '.css'
+            expand: false,
+            src: 'app/public/dev/styles/hgov.scss',
+            dest: 'app/public/min/styles/main.css'
         }]
     },
-    // Production settings
+
     prod: {
         options: {
-            outputStyle: 'compressed',
+            outputStyle: 'nested',
             sourceMap: false
         },
         files: [{
